6ea1f69fa61c7219f885b603ba3d6d8255e1b865
DAMS1875 <petersdamon8@gmail.com>
2024-04-23 15:30:05 +0200

    third

---
+++ about.js
@@ -0,0 +1,19 @@
+const user = document.getElementById('name');
+const bio = document.getElementById('bio');
+const education = document.getElementById('education');
+const year = document.getElementById('year');
+const sound = document.getElementById('sound');
+const pure = document.getElementById('pure');
+const Read = document.getElementById('read');
+const work = document.getElementById('work')
+
+
+
+user.textContent = "Rethabile Chomi";
+bio.textContent = "  I'm a movie lover who enjoys streaming movies.  My hobbies are working-out and cooking. Love reading books on nature and animals.";
+education.textContent = "Went to William Pescod High School";
+year.textContent = "Matriculated in 2022";
+sound.textContent = "Soundsytem"
+pure.textContent ="Pure html webcode"
+Read.textContent ="Iove reading book and telling stories"
+work .textContent ="I enjoy working out"
